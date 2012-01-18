b8e9726160e62176e75e9ad0fc4be98a64424681
Claude Noshpitz <claude.noshpitz@att.com>
2012-01-18 02:47:36 +0000

    default ops not yet working

--- make-files.rb
+++ make-files.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#! /usr/bin/env ruby
 
 require "open4"
 require "slop"
@@ -8,6 +8,11 @@ require "slop"
 BLOCKSIZE = 1024
 RANDBASE = 36
 RANDEXP = RANDBASE ** 8
+
+DEFAULT_DEST_DIR = '.'
+DEFAULT_NUM_FILES = 10
+DEFAULT_MAX_SIZE = 100
+
 
 #
 # return status, stdout
@@ -39,39 +44,50 @@ end
 #
 if __FILE__ == $0
 
-	dest_dir = "."
-	num_files = 10
-	max_size = 100    # blocks
-	policy = :random
+	# dest_dir = "."
+	# num_files = 10
+	# max_size = 100    # blocks
+	# policy = :random
 	
-	opts = Slop.parse do
-		on :d, :destdir=, 'destination directory'
-		on :n, :numfiles, 'number of files', num_files
-		on :s, :size, 'file size in #{BLOCKSIZE/1024)kB blocks', max_size
-		on :r, :random, 'random file sizes', optional: true # policy == :random
-		on :c, :constant, 'constant file sizes', optional: true # policy == :constant
+	
+	begin
+		opts = Slop.parse(:strict => true) do
+			on :d, :destdir=, 'destination directory', DEFAULT_DEST_DIR
+			on :n, :numfiles, 'number of files', :default => DEFAULT_NUM_FILES
+			on :z, :size, "file size in #{BLOCKSIZE/1024}kB blocks", :default => DEFAULT_MAX_SIZE
+			on :r, :random, 'random file sizes', optional: true # policy == :random
+			on :c, :constant, 'constant file sizes', optional: true # policy == :constant
+			banner "Usage: #{$0} [options]"
+			on :h, :help, 'get help' do 
+				puts help
+				exit 2
+			end
+		end
+	rescue Slop::InvalidOptionError => x
+		puts x.to_s
+		puts "Try --help for more info"
+		exit 1
 	end
-
 	dest_dir = opts[:destdir]
-	num_files = opts[:numfiles].to_i
-	max_size = opts[:size].to_i
-	policy = opts.random? || opts.constant?
-	# policy = opts.random? ? :random : :constant
+	num_files = opts[:numfiles].to_i 
+	max_size = opts[:size].to_i if opts[:size] 
+	policy = opts.random? ? :random : :constant
 
-	p dest_dir 
-	p num_files
-	p max_size
-	p policy 
+	# puts "policy: #{policy}"
+	# p dest_dir 
+	# p num_files
+	# p max_size
+	# p policy 
 
-	cmd = "mkdir -p #{dest_dir}"
+	# cmd = "mkdir -p #{dest_dir}"
 	# run(cmd)    # assume it worked if it didn't raise
 		
 	(1..num_files).each do |i|
 		size = policy == :constant ? max_size : rand(max_size)
-		name = "#{rand(RANDEXP).to_s(RANDBASE)}.#{i}"
-		puts name,size
+		name = "#{rand(RANDEXP).to_s(RANDBASE)}.#{i}"	# clever random filename
+		checksum = create_file("#{dest_dir}/#{name}", size)
+		puts "#{name}\t#{checksum}"
 	end
 
 
-	# create_file("a", 1000)
 end
